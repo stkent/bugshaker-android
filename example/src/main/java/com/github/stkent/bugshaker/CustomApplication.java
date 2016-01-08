1b6a680e5319a33bfca7bf52b6483e07240fde9e
Stuart Kent <stuart@detroitlabs.com>
2016-01-08 09:49:31 -0500

    Toast when shake is detected

--- example/src/main/java/com/github/stkent/bugshaker/CustomApplication.java
+++ example/src/main/java/com/github/stkent/bugshaker/CustomApplication.java
@@ -1,13 +1,33 @@
 package com.github.stkent.bugshaker;
 
 import android.app.Application;
+import android.hardware.SensorManager;
+import android.widget.Toast;
 
-public class CustomApplication extends Application {
+import com.squareup.seismic.ShakeDetector;
+
+public class CustomApplication extends Application implements ShakeDetector.Listener {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // TODO: initialize shake detection here
+        if (BuildConfig.DEBUG) {
+            final SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            final ShakeDetector shakeDetector = new ShakeDetector(this);
+            shakeDetector.start(sensorManager);
+        }
     }
+
+    @Override
+    public void hearShake() {
+        // TODO: prompt user; did they really want to submit a bug report?
+        // TODO: better to capture screenshot before or after prompt?
+        // TODO: how do we handle Maps?
+        // TODO: how do we handle activities that do not allow screenshots (for security purposes)?
+        // startActivity();
+
+        Toast.makeText(this, "Shake detected", Toast.LENGTH_SHORT).show();
+    }
+
 }
