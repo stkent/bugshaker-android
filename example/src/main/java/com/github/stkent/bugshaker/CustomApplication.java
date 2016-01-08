bc51c1f74a7c634652319ba40824a8288b61b4e3
Stuart Kent <stuart@detroitlabs.com>
2016-01-08 09:34:40 -0500

    Add custom application to example project

---
+++ example/src/main/java/com/github/stkent/bugshaker/CustomApplication.java
@@ -0,0 +1,13 @@
+package com.github.stkent.bugshaker;
+
+import android.app.Application;
+
+public class CustomApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // TODO: initialize shake detection here
+    }
+}
