f6031871257e4610d5e712210ae1d838ca27d7a1
Stuart Kent <stuart@detroitlabs.com>
2016-01-11 14:43:55 -0500

    Add placeholder lines for testing no screenshots

--- example/src/main/java/com/example/bugshaker/MainActivity.java
+++ example/src/main/java/com/example/bugshaker/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Placeholder for testing
+        // getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
     }
 
 }
