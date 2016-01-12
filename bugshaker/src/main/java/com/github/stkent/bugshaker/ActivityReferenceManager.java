f354afc2681c54042db270390ec2f17b41501e41
Stuart Kent <stuart@detroitlabs.com>
2016-01-11 19:43:45 -0500

    Add missing final

--- bugshaker/src/main/java/com/github/stkent/bugshaker/ActivityReferenceManager.java
+++ bugshaker/src/main/java/com/github/stkent/bugshaker/ActivityReferenceManager.java
@@ -23,7 +23,7 @@ import android.support.annotation.Nullable;
 
 import java.lang.ref.WeakReference;
 
-public class ActivityReferenceManager {
+public final class ActivityReferenceManager {
 
     @Nullable
     private WeakReference<Activity> wActivity;
