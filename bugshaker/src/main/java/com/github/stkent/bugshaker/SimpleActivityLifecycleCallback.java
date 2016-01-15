f3d3cc0606a41309f5ad179f7ab0cb186b6a49af
Stuart Kent <stuart@detroitlabs.com>
2016-01-15 11:26:57 -0500

    Make SimpleActivityLifecycleCallback less surprising

--- bugshaker/src/main/java/com/github/stkent/bugshaker/SimpleActivityLifecycleCallback.java
+++ bugshaker/src/main/java/com/github/stkent/bugshaker/SimpleActivityLifecycleCallback.java
@@ -23,27 +23,37 @@ import android.os.Bundle;
 public abstract class SimpleActivityLifecycleCallback implements Application.ActivityLifecycleCallbacks {
 
     @Override
-    public final void onActivityCreated(final Activity activity, final Bundle savedInstanceState) {
+    public void onActivityCreated(final Activity activity, final Bundle savedInstanceState) {
         // This method intentionally left blank
     }
 
     @Override
-    public final void onActivityStarted(final Activity activity) {
+    public void onActivityResumed(final Activity activity) {
         // This method intentionally left blank
     }
 
     @Override
-    public final void onActivityPaused(final Activity activity) {
+    public void onActivityStarted(final Activity activity) {
         // This method intentionally left blank
     }
 
     @Override
-    public final void onActivitySaveInstanceState(final Activity activity, final Bundle outState) {
+    public void onActivityPaused(final Activity activity) {
         // This method intentionally left blank
     }
 
     @Override
-    public final void onActivityDestroyed(final Activity activity) {
+    public void onActivityStopped(final Activity activity) {
+        // This method intentionally left blank
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(final Activity activity, final Bundle outState) {
+        // This method intentionally left blank
+    }
+
+    @Override
+    public void onActivityDestroyed(final Activity activity) {
         // This method intentionally left blank
     }
 
